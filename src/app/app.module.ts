1a9b92fc7aad26fff308cf464d5e9bfee574c0aa
Leonidas Peligrineli <l.peligrineli@comunique-se.com.br>
2021-06-18 18:08:43 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsModule } from './posts/posts.module';
+import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    PostsModule,
+    SharedModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule
+
+  ],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent
+  ]
+})
+export class SharedModule { }
